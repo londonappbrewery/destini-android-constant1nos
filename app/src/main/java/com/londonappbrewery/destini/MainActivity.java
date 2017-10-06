ea5f338d2159b661ec1eb411403ad78f76cb12d3
dinob <dinobatzio@gmail.com>
2017-10-06 13:03:43 +0300

    This is my version

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,11 +1,32 @@
 package com.londonappbrewery.destini;
 
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TheStory story = new TheStory();
+    TextView text;
+    Button upButton, downButton;
+    int keepTrack=0, pressedButton=0;
+    final int[] mStoryIndex = {
+            R.string.T1_Story,
+            R.string.T1_Ans1,
+            R.string.T1_Ans2,
+            R.string.T2_Story,
+            R.string.T2_Ans1,
+            R.string.T2_Ans2,
+            R.string.T3_Story,
+            R.string.T3_Ans1,
+            R.string.T3_Ans2,
+            R.string.T4_End,
+            R.string.T5_End,
+            R.string.T6_End};
 
 
     @Override
@@ -15,16 +36,41 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        text = (TextView) findViewById(R.id.storyTextView);
+        upButton = (Button) findViewById(R.id.buttonTop);
+        downButton = (Button) findViewById(R.id.buttonBottom);
 
+        upButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressedButton=1;
+                story.setTrack(keepTrack, pressedButton);
+                keepTrack=story.getmTrack();
+                SetStoryText(keepTrack);
+            }
+        });
 
+        downButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressedButton=2;
+                story.setTrack(keepTrack, pressedButton);
+                keepTrack=story.getmTrack();
+                SetStoryText(keepTrack);
+            }
+        });
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+    }
 
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
-
+    public void SetStoryText(int mKeepTrack) {
+        text.setText(mStoryIndex[mKeepTrack]);
+        if(mKeepTrack <= 8) {
+            upButton.setText(mStoryIndex[mKeepTrack+1]);
+            downButton.setText(mStoryIndex[mKeepTrack+2]);
+        }
+        else {
+            upButton.setVisibility(View.GONE);
+            downButton.setVisibility(View.GONE);
+        }
     }
 }
